2db952edb6e3dd4ac00f74d929e4d10ca106a672
LeeSeongSu <oplm123456@naver.com>
2020-03-26 19:55:10 +0900

    구구단 만들기

---
+++ 알고리즘/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a;
+
+		a = sc.nextInt();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(a + " * " + i + " = " + (a * i));
+		}
+
+	}
+}
