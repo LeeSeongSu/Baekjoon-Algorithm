1c886316369577009c076f9ff371794f729b176f
LeeSeongSu <oplm123456@naver.com>
2020-03-26 20:08:46 +0900

    1부터 n까지 합

---
+++ 알고리즘/src/sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a, sum = 0;
+
+		a = sc.nextInt();
+
+		for (int i = 1; i <= a; i++) {
+			sum += i;
+
+		}
+		System.out.println(sum);
+	}
+}
