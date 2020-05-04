bf66286cd440c8e7005c006477da194815bb7dd2
LeeSeongSu <oplm123456@naver.com>
2020-05-04 20:02:47 +0900

    2742 기찍 N(for문 생기초)

---
+++ 알고리즘/src/baekjoon2742.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class baekjoon2742 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = N; i > 0; i--) {
+			System.out.println(i);
+
+		}
+	}
+
+}
