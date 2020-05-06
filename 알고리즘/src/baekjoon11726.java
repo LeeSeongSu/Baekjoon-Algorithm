8688cbb1ca0e2fdef405490e8d25ab7356517ded
LeeSeongSu <oplm123456@naver.com>
2020-05-06 22:56:25 +0900

    11726번 2Xn 타일링 (dp문제)

---
+++ 알고리즘/src/baekjoon11726.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class baekjoon11726 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int dp[] = new int[n + 1];
+
+		for (int i = 0; i <= n; i++) {
+			if (i <= 2)
+				dp[i] = i;
+			else
+
+				dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+		}
+
+		System.out.println(dp[n]);
+	}
+
+}
