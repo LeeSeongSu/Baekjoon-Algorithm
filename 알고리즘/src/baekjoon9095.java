004a21c6ccd64a9d260c5eb87367600d05093de2
LeeSeongSu <oplm123456@naver.com>
2020-05-07 20:00:14 +0900

    9095번 1,2,3더하기 ( dP활용)

---
+++ 알고리즘/src/baekjoon9095.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class baekjoon9095 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int dp[] = new int[11];
+
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+
+		for (int i = 0; i < T; i++) {
+			int n = sc.nextInt();
+
+			for (int j = 4; j <= n; j++) {
+
+				dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+
+			}
+			System.out.println(dp[n]);
+		}
+
+	}
+
+}
